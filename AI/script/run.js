0018b76a8dd6be80d24df8a8900bb4a9bfbee317
Jacob Bondley <hasharahha@gmail.com>
2014-12-09 19:30:22 -0500

    Update run.js

--- AI/script/run.js
+++ AI/script/run.js
@@ -3,7 +3,8 @@ var ai =[];
 var fallback ="/";
 
 function Run(){
-ai[0] = document.createElement('input');
-ai[0].setAttribute("style", "width: 600px; height: 300px;");
-document.body.appendChild(ai[0]);
+ai[1] = document.createElement('input');
+ai[1].setAttribute("style", "width: 500px; height: 100px; font-size: 80px; color: white; background-color: black;");
+ai[1].setAttribute("id", "commandprompt");
+document.body.appendChild(ai[1]);
 }
