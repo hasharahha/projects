7e5ccab35b8663e1c16d917f654a2edb7314438a
Jacob Bondley <hasharahha@gmail.com>
2014-12-09 19:16:46 -0500

    Create run.js

---
+++ AI/script/run.js
@@ -0,0 +1,8 @@
+var ai ={};
+var fallback ="/";
+
+function Run(){
+ai.input = document.createElement('input');
+ai[input].setAttribute("style", "width: 600px; height: 300px;");
+document.body.appendChild(ai[input]);
+}
